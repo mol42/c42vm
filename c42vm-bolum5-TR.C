0a454353b215ab64e1cb6ad5a27e9ba7aa1633cc
Tayfun YUGRUK <tayfunyugruk@gmail.com>
2023-10-26 01:57:11 -0400

    header imports improved

--- c42vm-bolum5-TR.C
+++ c42vm-bolum5-TR.C
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define STACK_SIZE 100
 
--- cv42vm-full-TR.c
+++ cv42vm-full-TR.c
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 // ONEMLI NOT :
 // BU SANAL ISLEMCI SADECE INTEGER SAYILARLA ISLEM YAPMAK UZERE
